7d726441b55fd7de1c8ee9dc41d4f969010cffbd
Priyanshu <priyanshu24pandey@gmail.com>
2024-01-10 18:40:24 +0530

    resolved ntlify warning

--- src/sarvamitra-listing/DetailsPage.jsx
+++ src/sarvamitra-listing/DetailsPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 
 const DetailsPage = () => {
   const { id } = useParams();
+  console.log(id);
   return (
     <div className="p-5">
       <h1 className="text-2xl mb-4 font-semibold">Implement Details Page</h1>
