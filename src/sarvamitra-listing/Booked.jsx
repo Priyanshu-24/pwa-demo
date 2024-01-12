2ab56b884c3ba42af6f9373ba28249931f34eb85
Priyanshu <priyanshu24pandey@gmail.com>
2024-01-12 11:28:20 +0530

    noti enabled

--- src/sarvamitra-listing/Booked.jsx
+++ src/sarvamitra-listing/Booked.jsx
@@ -1,7 +1,30 @@
 const Booked = () => {
+  const registration = navigator.serviceWorker.getRegistration();
+  const sendNotification = async () => {
+    if (Notification.permission === "granted") {
+      showNotification();
+    } else {
+      if (Notification.permission !== "denied") {
+        const permission = await Notification.requestPermission();
+
+        if (permission === "granted") {
+          showNotification();
+        }
+      }
+    }
+  };
+
+  const showNotification = () => {
+    if ("showNotification" in registration) {
+      registration.showNotification("Hello");
+    } else {
+      new Notification("Hello");
+    }
+  };
+
   return (
     <div className="p-5">
-      <h1 className="text-2xl mb-4 font-semibold">
+      <h1 className="text-2xl mb-4 font-semibold" onClick={sendNotification}>
         Implement Booked Successfully
       </h1>
     </div>
